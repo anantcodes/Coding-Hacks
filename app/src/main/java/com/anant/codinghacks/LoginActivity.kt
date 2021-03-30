0e3bb16bfcf0a8af1b4ea39cb9cea42dfb599964
Anant Kanchan <anant4265@gmail.com>
2021-03-30 11:50:59 +0530

    vars declared and initialised

--- app/src/main/java/com/anant/codinghacks/LoginActivity.kt
+++ app/src/main/java/com/anant/codinghacks/LoginActivity.kt
@@ -2,10 +2,23 @@ package com.anant.codinghacks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
+
+
+
+    lateinit var etEmail:EditText
+    lateinit var etPassword:EditText
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        etEmail=findViewById(R.id.etEmail)
+        etPassword=findViewById(R.id.etPassword)
+        
     }
 }
