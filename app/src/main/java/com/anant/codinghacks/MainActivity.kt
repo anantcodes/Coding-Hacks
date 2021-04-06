916bd3393ab710699b423031bf7be3d85af85c0e
Anant Kanchan <anant4265@gmail.com>
2021-04-06 14:04:48 +0530

    intent 1 added

--- app/src/main/java/com/anant/codinghacks/MainActivity.kt
+++ app/src/main/java/com/anant/codinghacks/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.anant.codinghacks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnDev:Button
+    lateinit var btnPL:Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDev=findViewById(R.id.btnDev)
+        btnPL=findViewById(R.id.btnPL)
+
+        btnDev.setOnClickListener{
+            intent = Intent(applicationContext, DevelopmentActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
     }
 }
