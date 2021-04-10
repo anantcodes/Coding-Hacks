bf5794e603589018f468d2682a6298c4451da501
Anant Kanchan <anant4265@gmail.com>
2021-04-10 21:55:47 +0530

    progress bar backend connected

--- app/src/main/java/com/anant/codinghacks/ProgressActivity.kt
+++ app/src/main/java/com/anant/codinghacks/ProgressActivity.kt
@@ -1,15 +1,29 @@
 package com.anant.codinghacks
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 
 class ProgressActivity : AppCompatActivity() {
+
+
+
+
+    lateinit var progressBar:ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_progress)
 
-// to implement Progress bar dynamically
+// to implement Progress bar
 
+        progressBar=findViewById(R.id.progressBar)
+        progressBar.max=1000
+        val currentProgress=600
+        ObjectAnimator.ofInt(progressBar,"progress",currentProgress)
+                .setDuration(2000)
+                .start()
 
     }
 }
