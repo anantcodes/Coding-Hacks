0307f6366b6709dcef07e7fa05c8755687b0fcd5
Anant Kanchan <anant4265@gmail.com>
2021-04-18 18:36:21 +0530

    10 v dec&izd

--- app/src/main/java/com/anant/codinghacks/QuestionsActivity.kt
+++ app/src/main/java/com/anant/codinghacks/QuestionsActivity.kt
@@ -11,6 +11,15 @@ class QuestionsActivity : AppCompatActivity() {
 
 
     lateinit var Ans1:EditText
+    lateinit var Ans2:EditText
+    lateinit var Ans3:EditText
+    lateinit var Ans4:EditText
+    lateinit var Ans5:EditText
+    lateinit var Ans6:EditText
+    lateinit var Ans7:EditText
+    lateinit var Ans8:EditText
+    lateinit var Ans9:EditText
+    lateinit var Ans10:EditText
     lateinit var btnSubmit:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +27,15 @@ class QuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_questions)
 
         Ans1=findViewById(R.id.Ans1)
+        Ans2=findViewById(R.id.Ans2)
+        Ans3=findViewById(R.id.Ans3)
+        Ans4=findViewById(R.id.Ans4)
+        Ans5=findViewById(R.id.Ans5)
+        Ans6=findViewById(R.id.Ans6)
+        Ans7=findViewById(R.id.Ans7)
+        Ans8=findViewById(R.id.Ans8)
+        Ans9=findViewById(R.id.Ans9)
+        Ans10=findViewById(R.id.Ans10)
         btnSubmit=findViewById(R.id.btnSubmit)
 
         btnSubmit.setOnClickListener {
