ae3faabb6def9fca2d665010102e249854eaece6
Anant Kanchan <anant4265@gmail.com>
2021-04-25 11:35:48 +0530

    progress bar reset implemented

--- app/src/main/java/com/anant/codinghacks/ProgressActivity.kt
+++ app/src/main/java/com/anant/codinghacks/ProgressActivity.kt
@@ -3,6 +3,7 @@ package com.anant.codinghacks
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ProgressBar
 
 class ProgressActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class ProgressActivity : AppCompatActivity() {
 
 
     lateinit var progressBar:ProgressBar
+    lateinit var btnReset:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +21,21 @@ class ProgressActivity : AppCompatActivity() {
 // to implement Progress bar
 
         progressBar=findViewById(R.id.progressBar)
+        btnReset=findViewById(R.id.btnReset)
+
         progressBar.max=1000
         val currentProgress=600
         ObjectAnimator.ofInt(progressBar,"progress",currentProgress)
                 .setDuration(2000)
                 .start()
+
+        btnReset.setOnClickListener {
+            progressBar.max=1000
+            val cProgress=0
+            ObjectAnimator.ofInt(progressBar,"progress",cProgress)
+                    .setDuration(2000)
+                    .start()
+        }
 
     }
 }
