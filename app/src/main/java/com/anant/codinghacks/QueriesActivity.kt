afcd15dcfb2d76a35cdc5df3454df4cc7ec37be8
Anant Kanchan <anant4265@gmail.com>
2021-04-25 12:47:09 +0530

    Queries et done

--- app/src/main/java/com/anant/codinghacks/QueriesActivity.kt
+++ app/src/main/java/com/anant/codinghacks/QueriesActivity.kt
@@ -2,10 +2,20 @@ package com.anant.codinghacks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class QueriesActivity : AppCompatActivity() {
+
+    lateinit var btnQue:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_queries)
+
+        btnQue=findViewById(R.id.btnQue)
+        btnQue.setOnClickListener {
+            Toast.makeText(this,"Query Submitted", Toast.LENGTH_SHORT).show()
+        }
     }
 }
