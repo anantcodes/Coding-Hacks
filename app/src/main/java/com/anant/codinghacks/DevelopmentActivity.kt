664f166df2b39665e5a097bae09400259be0468d
Anant Kanchan <anant4265@gmail.com>
2021-04-25 10:50:12 +0530

    web dev backend

--- app/src/main/java/com/anant/codinghacks/DevelopmentActivity.kt
+++ app/src/main/java/com/anant/codinghacks/DevelopmentActivity.kt
@@ -1,11 +1,37 @@
 package com.anant.codinghacks
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DevelopmentActivity : AppCompatActivity() {
+
+
+
+    lateinit var btnWeb: Button
+    lateinit var btnMobile: Button
+    lateinit var btnDS: Button
+    lateinit var btnAPI: Button
+    lateinit var btnSSD: Button
+    lateinit var btnCC: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_development)
+
+        btnWeb=findViewById(R.id.btnWeb)
+        btnMobile=findViewById(R.id.btnMobile)
+        btnDS=findViewById(R.id.btnDS)
+        btnAPI=findViewById(R.id.btnAPI)
+        btnSSD=findViewById(R.id.btnSSD)
+        btnCC=findViewById(R.id.btnCC)
+
+        btnWeb.setOnClickListener {
+
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.geeksforgeeks.org/can-start-learn-web-development/")))
+
+        }
     }
 }
