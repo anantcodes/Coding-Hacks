52e7b94a2be629296c309e1deac2fc8e59d84702
Anant Kanchan <anant4265@gmail.com>
2021-04-25 11:20:17 +0530

    java backend

--- app/src/main/java/com/anant/codinghacks/LanguageActivity.kt
+++ app/src/main/java/com/anant/codinghacks/LanguageActivity.kt
@@ -1,11 +1,42 @@
 package com.anant.codinghacks
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LanguageActivity : AppCompatActivity() {
+
+    lateinit var btnJava: Button
+    lateinit var btnCPlus: Button
+    lateinit var btnC: Button
+    lateinit var btnPython: Button
+    lateinit var btnKotlin: Button
+    lateinit var btnJS: Button
+    lateinit var btnGo: Button
+    lateinit var btnCS: Button
+    lateinit var btnHtml: Button
+    lateinit var btnCSS: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language)
+
+        btnJava=findViewById(R.id.btnJava)
+        btnCPlus=findViewById(R.id.btnCPlus)
+        btnC=findViewById(R.id.btnC)
+        btnPython=findViewById(R.id.btnPython)
+        btnKotlin=findViewById(R.id.btnKotlin)
+        btnJS=findViewById(R.id.btnJS)
+        btnGo=findViewById(R.id.btnGo)
+        btnCS=findViewById(R.id.btnCS)
+        btnHtml=findViewById(R.id.btnHtml)
+        btnCSS=findViewById(R.id.btnCSS)
+
+        btnJava.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.geeksforgeeks.org/java-tutorials/")))
+        }
+
     }
 }
