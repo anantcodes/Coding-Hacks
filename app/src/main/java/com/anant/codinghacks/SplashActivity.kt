be88c419dbcc4ec65b175077c57cbc114d66d19f
Anant Kanchan <anant4265@gmail.com>
2021-04-25 14:07:04 +0530

    splash backend done

--- app/src/main/java/com/anant/codinghacks/SplashActivity.kt
+++ app/src/main/java/com/anant/codinghacks/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.anant.codinghacks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(
+                Intent(this,
+                    LoginActivity::class.java)
+            )
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
